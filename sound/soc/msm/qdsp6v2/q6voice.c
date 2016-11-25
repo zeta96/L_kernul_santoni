ee374e742c8932980ae60a9e05f248db39e3e9af
Sultanxda <sultanxda@gmail.com>
2016-11-25 09:40:00 -0800

    msm: qpnp-haptic: Disable haptics for voice calls and video recordings
    Haptic feedback is intrusive during phone calls and video recordings,
    especially due to the noise it generates.
    
    Add hooks to disable haptics for these use cases to improve UX.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- sound/soc/msm/qdsp6v2/q6voice.c
+++ sound/soc/msm/qdsp6v2/q6voice.c
@@ -17,6 +17,7 @@
 #include <linux/wait.h>
 #include <linux/mutex.h>
 #include <linux/msm_audio_ion.h>
+#include <linux/qpnp/qpnp-haptic.h>
 
 #include <soc/qcom/socinfo.h>
 #include <linux/qdsp6v2/apr_tal.h>
@@ -5874,6 +5875,8 @@ int voc_end_voice_call(uint32_t session_id)
 		ret = -EINVAL;
 	}
 
+	qpnp_disable_haptics(false);
+
 	mutex_unlock(&v->lock);
 	return ret;
 }
@@ -6228,6 +6231,8 @@ int voc_start_voice_call(uint32_t session_id)
 		ret = -EINVAL;
 		goto fail;
 	}
+
+	qpnp_disable_haptics(true);
 fail:
 	mutex_unlock(&v->lock);
 	return ret;
